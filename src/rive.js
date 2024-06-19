df5c585114091896098d5988464bd34cdc1bd1b2
Guillaume <guillaume@MBP12.local>
2024-06-19 15:04:06 +0200

    switch to statemachine inside a variable

--- src/rive.js
+++ src/rive.js
@@ -1,15 +1,17 @@
+const stateMachine = "State Machine 1";
+
 const riveInstance = new rive.Rive({
 	src: "src/.riv", //get rive file
 	canvas: document.getElementById("rive"), //get correct canvas
 	autoplay: true,
-	stateMachines: "State Machine 1", // get correct stateMachine
+	stateMachines: stateMachine, // get correct stateMachine
 	automaticallyHandleEvents: true, // Automatically handle RiveHTTPEvents
 
 	onLoad: () => {
 		// Prevent a blurry canvas by using the device pixel ratio
 		riveInstance.resizeDrawingSurfaceToCanvas();
 
-		const inputs = riveInstance.stateMachineInputs("State");
+		const inputs = riveInstance.stateMachineInputs(stateMachine);
 		// myInput = inputs.find((i) => i.name === inputName);
 		// myInput.value = true;
 
